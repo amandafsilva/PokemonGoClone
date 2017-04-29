9540d0ca4a7af125ce8fb4c9c772e6e311b088b5
Amanda Silva <amandafsilva@outlook.com>
2017-04-29 13:52:56 -0300

    Login parcialmente implementado

--- app/src/main/java/amandafsilva/pokemongoclone/LoginActivity.java
+++ app/src/main/java/amandafsilva/pokemongoclone/LoginActivity.java
@@ -1,27 +1,66 @@
 package amandafsilva.pokemongoclone;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public PokemonGoCloneDB bd;
+
+    EditText edtUsername;
+    EditText edtSenha;
+    TextView erroLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bd = new PokemonGoCloneDB();
+
+        edtUsername = (EditText) findViewById(R.id.editText_Usuario);
+        edtSenha = (EditText) findViewById(R.id.editText_Senha);
+        erroLogin = (TextView) findViewById(R.id.textView_Erro);
     }
 
     public void login(View view) {
-        EditText username = (EditText) findViewById(R.id.editText_Usuario);
-        EditText senha = (EditText) findViewById(R.id.editText_Senha);
+        // Reseta os erros
+        edtUsername.setError(null);
+        edtSenha.setError(null);
 
-        if(username.getText().toString().equals("admin") && senha.getText().toString().equals("admin")) {
-            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-            startActivity(intent);
-            finish();
+        // Passa os valores do EditText pra String e guarda
+        String username = edtUsername.getText().toString();
+        String senha = edtSenha.getText().toString();
+
+        boolean cancelar = false;
+        View focusView = null;
+
+        // Checa se o campo Senha está vazio
+        if (TextUtils.isEmpty(senha)) {
+            edtSenha.setError(getString(R.string.erro_campo_obrigatório));
+            focusView = edtSenha;
+            cancelar = true;
+        }
+
+        // Checa se o campo Usuário está vazio
+        if (TextUtils.isEmpty(username)) {
+            edtUsername.setError(getString(R.string.erro_campo_obrigatório));
+            focusView = edtUsername;
+            cancelar = true;
+        }
+
+        if (cancelar) {
+            // Houve erro no preenchimento dos campos
+            // Não prosseguir com o login e focar nos campos inválidos
+            focusView.requestFocus();
+        } else {
+            
         }
     }
 
